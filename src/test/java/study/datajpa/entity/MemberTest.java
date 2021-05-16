ee87cd423c0e25ccededcd68317aeb88e0b942c6
yoo-jaein <y_jaein@naver.com>
2021-05-16 16:49:08 +0900

    Initial commit

---
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,49 @@
+package study.datajpa.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+public class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 19, teamA);
+        Member member2 = new Member("member2", 29, teamA);
+        Member member3 = new Member("member3", 39, teamB);
+        Member member4 = new Member("member4", 49, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //초기화
+        em.flush(); //강제로 db에 insert날림
+        em.clear(); //em 캐시 초기화
+
+        //확인
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team = " + member.getTeam());
+        }
+    }
+}
